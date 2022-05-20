a1f90ea186fc12a2aaa7f51c1ae2f182a10633e0
ERDEM UÇAK <52521399+erdemucak@users.noreply.github.com>
2022-05-20 08:57:44 +0300

    Add files via upload

---
+++ AccessRightsCalendar.ps1
@@ -0,0 +1,22 @@
+﻿# Use Exchange Management Shell for the Script 
+
+#Access rights Editor for the specific user 
+add-mailboxfolderpermission -identity yourname@domain.com.tr:\Calendar -user "Username" -accessrights Editor
+
+#Access rights reviewer for the specific user 
+add-mailboxfolderpermission -identity yourname@domain.com.tr:\Calendar -user "Username" -accessrights  reviewer
+
+#Access rights owner for the specific user 
+add-mailboxfolderpermission -identity yourname@domain.com.tr:\Calendar -user "Username" -accessrights owner
+
+#Access rights contributor for the specific user 
+add-mailboxfolderpermission -identity yourname@domain.com.tr:\Calendar -user "Username" -accessrights contributor
+
+#View the Calendar Share for the specific User's
+get-mailboxfolderpermission -identity yourname@domain.com.tr:\calendar
+
+
+Set-MailboxFolderPermission –Identity user@domain.com:\Calendar –User “alias” –AccessRights Owner
+
+
+Remove-MailboxFolderPermission –Identity user@domain.com:\Calendar –User “hedef kullanıcı takma adı” –AccessRights Owner
